555bd0bc25687d70f60c79751b46611fa042e47a
Netesoo <milosz.p04@gmail.com>
2025-07-16 23:08:28 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@ import socket  # noqa: F401
 
 
 def main():
+    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    
+
+    while True:
+        request: bytes = connection.recv(1024)
+        data: str = request.decode()
+
+
+        if "ping" in data.lower():
+            connection.sendall("+PONG\r\n".encode())
     
 
 if __name__ == "__main__":
