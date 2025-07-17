20198e4dbb438f20c394d93d1d38f81d8ae292fe
Netesoo <milosz.p04@gmail.com>
2025-07-17 21:27:11 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 import socket  # noqa: F401
+import threading
+
+buff_size: int = 1024
+
+
+def handle_client(client: socket.socket):
+        while data := client.recv(buff_size):
+            if data == b"":
+                break
+
+            client.sendall("+PONG\r\n".encode())
+
+
+def _gether():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 
 def main():
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-    
-
-    while True:
-        request: bytes = connection.recv(1024)
-        data: str = request.decode()
-
-
-        if "ping" in data.lower():
-            connection.sendall("+PONG\r\n".encode())
-    
+    _gether()
 
 if __name__ == "__main__":
     main()
